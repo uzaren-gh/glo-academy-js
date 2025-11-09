cffb50db384d956b9dd196ce02ac328266b19443
Sergey Burenkov <uzaren@ukr.net>
2025-11-09 22:21:02 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const contents = document.querySelectorAll('.program-line__content');
+
+contents.forEach(element => {
+    const title = element.querySelector('.program-line__title');
+    const descr = element.querySelector('.program-line__descr');
+
+    title.addEventListener('click', () => {
+        descr.classList.toggle('active');
+
+        contents.forEach(el => {
+            const d = el.querySelector('.program-line__descr');
+            if (d != descr) { d.classList.remove('active') }
+        })
+
+
+    })
+
+})
